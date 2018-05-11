c7b223ddac70d69bcd8058357dcff5a307d33cec
arthur16 <artbscott16@gmail.com>
2018-05-10 19:11:30 -0500

    second commit

---
+++ resume.js
@@ -0,0 +1,28 @@
+function myName(n) {
+    console.log("Name:" + n);
+}
+myName(' ARTHUR SCOTT');
+function myCareer(c) {
+    console.log("Career: " + c);
+}
+myCareer('Full Stack Engineer/Developer');
+function myDescription(d) {
+    console.log("Description: " + d);
+}
+myDescription("I like basketball");
+
+var interests = ['Sports', 'Cars', 'Food', 'Music'];
+function myInterests() {
+for (var i = 0; i < interests.length; i++); {
+            return console.log("My Interests:");
+            console.log(interests[i]);
+        }
+    }
+myInterests()
+// figure out issue with this for loop function
+
+function displayPosition(job, comp, desc) {
+    console.log(job + ' at ' + comp + ' - ' + desc);
+}
+displayPosition("Math tutor", "Ramsay High School", "Tutored students")
+// Stopping point
